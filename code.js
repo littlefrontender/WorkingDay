f0fb69dbd8af7c743d3c368873ea37cabb0ae36a
Valentin Tsvetkov <79913874+littlefrontender@users.noreply.github.com>
2021-10-21 14:55:15 +0300

    numbers() conditions() and other fixes

--- code.js
+++ code.js
@@ -1,10 +1,23 @@
-let dayNumber= document.querySelector('.days'); // Дни
-let monthInput = document.querySelector('.month'); // Месяцы
-let yearInput = document.querySelector('.year'); // Годы
-let button = document.querySelector('.button'); // Кнопка
+const dayNumber = document.querySelector('.days'); // Дни
+const monthInput = document.querySelector('.month'); // Месяцы
+const yearInput = document.querySelector('.year'); // Годы
+const button = document.querySelector('.button'); // Кнопка
 let firstWorkDay = []; // Ряд первых рабочих дней с начала года
 let secondWorkDay = []; // Ряд вторых рабочих дней с начала года
 
+// Создаём числа дат в верстке
+
+function number(parentSelector) {
+    let option;
+
+    for (let i=1; i<=31; i++) {
+        option = document.createElement('option');
+        option.innerHTML = `${i}`;
+        parentSelector.append(option);
+    }
+}
+
+number(dayNumber);
 
 // Создаём ряды. Функция принимает год и отдаёт ряды рабочих дней
 
@@ -12,60 +25,63 @@ function yearArray (year) {
     switch (year) {
         case '2021': 
         for (var i=3;i<366;i+=4) {
-            firstWorkDay.push(i)
-            if (i!==365)
-            secondWorkDay.push(i+1)
-        };
+            firstWorkDay.push(i);
+            if (i!==365) {
+            secondWorkDay.push(i+1);
+            }
+        }
         break;
         case '2022':
-            for (var i=2;i<366;i+=4) {
-                firstWorkDay.push(i)
-                if (i!==365)
-                secondWorkDay.push(i+1)
-            };
+            for (var j=2;j<366;j+=4) {
+                firstWorkDay.push(j);
+                if (j!==365) {
+                secondWorkDay.push(j+1);
+                }
+            }
             break;
         case '2023':
-            for (var i=1;i<366;i+=4) {
-                firstWorkDay.push(i)
-                if (i!==365)
-                secondWorkDay.push(i+1)
-            };
+            for (var k=1;k<366;k+=4) {
+                firstWorkDay.push(k);
+                if (k!==365) {
+                secondWorkDay.push(k+1);
+                }
+            }
             break;
     }
 }
 
-// Функция принимает дату и дату и отдаёт число дня с начала года
+const january = 0;
+const february = 31;
+const march = february + 28;
+const april = march + 31;
+const may = april + 30;
+const june = may + 31;
+const july = june + 30;
+const august = july + 31;
+const september = august + 31;
+const october = september + 30;
+const november = october + 31;
+const december = november + 30;
 
-let january=0;
-let february=31;
-let march=february+28;
-let april=march+31;
-let may=april+30;
-let june=may+31;
-let july=june+30;
-let august=july+31;
-let september=august+31;
-let october=september+30;
-let november=october+30;
-let december=november+30;
+const daysInMonth = {
+    'january': january,
+    'february': february,
+    'march': march,
+    'april': april,
+    'may': may,
+    'june': june,
+    'july': july,
+    'august': august,
+    'september': september,
+    'october': october,
+    'november': november,
+    'december': december
+};
 
-let daysInMonth = {
-    'january':january,
-    'february':february,
-    'march':march,
-    'april':april,
-    'may':may,
-    'june':june,
-    'july':july,
-    'august':august,
-    'september':september,
-    'october':october,
-    'november':november,
-    'december':december
-}
+// Функция принимает дату и отдаёт число дня с начала года
 
 function dayInYear(day, month) {
-    return daysInMonth[month] + Number(day)
+    return daysInMonth[month] + Number(day);
 }
 
 // Сравние рядов для определения выходного и рабочего дня
@@ -76,20 +92,37 @@ function application(x) {
         
         if (firstWorkDay[j]===x) {
             result = 'Это первый рабочий день';
-            return result
+            return result;
         } else if (secondWorkDay[j]===x) {
             result = 'Это второй рабочий день';
-            return result
+            return result;
         }
     }
-    return result
+    return result;
+}
+
+// Создаём определенные условия, когда программа должна выдавать ошибку
+
+function conditions () {
+    if (dayNumber.value == 29 && monthInput.value === 'february' ||
+        dayNumber.value == 30 && monthInput.value === 'february' ||
+        dayNumber.value == 31 && monthInput.value === 'february' ||
+        dayNumber.value == 31 && monthInput.value === 'april' ||
+        dayNumber.value == 31 && monthInput.value === 'june' ||
+        dayNumber.value == 31 && monthInput.value === 'september' ||
+        dayNumber.value == 31 && monthInput.value === 'november'
+    ) {
+        alert('Введена неправильная дата!');
+    } else {
+        alert(application(dayInYear(dayNumber.value, monthInput.value)));
+    }
 }
 
 // Действия по клику
 
 button.addEventListener('click', function() {
     yearArray(yearInput.value);
-    alert(application(dayInYear(dayNumber.value, monthInput.value)));
+    conditions();
     firstWorkDay = []; // обнуляем ряд
     secondWorkDay = []; // обнуляем ряд
-})
+});
